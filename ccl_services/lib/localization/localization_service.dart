8cc72253ad3aa6ac8d6d86b4150a5a227e748e8c
Pasan <pasaner@brownsgroup.com>
2024-05-28 06:20:55 +0530

    docs(Code): Add dart doc comments

--- ccl_services/lib/localization/localization_service.dart
+++ ccl_services/lib/localization/localization_service.dart
@@ -1,16 +1,22 @@
 part of '../ccl_services.dart';
 
+/// A service that manages the application's locale.
 class LocalizationService implements InitializableDependency {
+  /// Logging tag for this service.
   // ignore: constant_identifier_names
   static const String TAG = 'LocalizationService';
 
+  /// The secure storage service used to persist the locale.
   final SecureStorageService _secureStorageService =
-      StackedLocator.instance.get();
+  StackedLocator.instance.get();
 
+  /// The controller for the current locale.
   late final BehaviorSubject<Locale> _localeController;
 
+  /// A stream of the current locale.
   BehaviorSubject<Locale> get localeController => BehaviorSubject();
 
+  /// Initializes the service by loading the locale from secure storage.
   @override
   Future<void> init() async {
     final val = await _secureStorageService.locale.read();
@@ -18,5 +24,6 @@ class LocalizationService implements InitializableDependency {
     _localeController = BehaviorSubject.seeded(locale);
   }
 
+  /// Updates the current locale and persists it to secure storage.
   void onLocaleChanged(Locale locale) => _localeController.add(locale);
 }
